d9777dc0035122aef65579949263890a4cb1f73d
CanIBuyYouAHat <cironoc5@gmail.com>
2024-04-26 13:58:45 -0400

    Modal

--- kanban-app-tutorial/src/redux/features/appSlice.ts
+++ kanban-app-tutorial/src/redux/features/appSlice.ts
@@ -13,17 +13,17 @@ export const features = createSlice({
     // Functions that update the initialState are written inside the reducers object
     reducers: {
         // This function updates the board name when called
-        setPageTitle: (state, action: PayloadAction<string>) => {
+        setCurrentBoardName: (state, action: PayloadAction<string>) => {
             state.currentBoardName = action.payload;
         },
     },
 });
 
 // Export the functions defined inside the reducers here
-export const { setPageTitle } = features.actions;
+export const { setCurrentBoardName } = features.actions;
 
 // Selector function to retrieve the current board name from the state
-export const getPageTitle = (state: RootState) => state.features.currentBoardName;
+export const getCurrentBoardName = (state: RootState) => state.features.currentBoardName;
 
 // Export the reducer for use in the Redux store
 export default features.reducer;
